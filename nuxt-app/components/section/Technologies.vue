9e5f1ae9c5f2aa9fe35c6994367ea9e2b27495a4
Łukasz Fujarski <lukasion@gmail.com>
2024-01-04 23:06:52 +0100

    Add animated technology items

--- nuxt-app/components/section/Technologies.vue
+++ nuxt-app/components/section/Technologies.vue
@@ -17,45 +17,65 @@
 			</p>
 
 			<div class="technology__container mt-12">
-				<div class="technology__item">
-					<Icon name="logos:vue" size="48px"/>
-					Vue.js
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item">
+						<Icon name="logos:vue" size="48px"/>
+						Vue.js
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:laravel" size="48px"/>
-					Laravel
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: .6s">
+						<Icon name="logos:laravel" size="48px"/>
+						Laravel
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:nuxt-icon" size="48px"/>
-					Nuxt
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: .9s">
+						<Icon name="logos:nuxt-icon" size="48px"/>
+						Nuxt
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:nodejs" size="48px"/>
-					Node.js
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: 1.2s">
+						<Icon name="logos:nodejs" size="48px"/>
+						Node.js
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:php-alt" size="48px"/>
-					PHP
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: 1.5s">
+						<Icon name="logos:php-alt" size="48px"/>
+						PHP
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="vscode-icons:file-type-scss2" size="48px"/>
-					SCSS
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: 1.7s">
+						<Icon name="vscode-icons:file-type-scss2" size="48px"/>
+						SCSS
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:tailwindcss-icon" size="48px"/>
-					Tailwind
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: 1.9s">
+						<Icon name="logos:tailwindcss-icon" size="48px"/>
+						Tailwind
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:docker-icon" size="48px"/>
-					Docker
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: 2.1s">
+						<Icon name="logos:docker-icon" size="48px"/>
+						Docker
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:git" size="48px"/>
-					Git
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: 2.3s">
+						<Icon name="logos:git" size="48px"/>
+						Git
+					</span>
 				</div>
-				<div class="technology__item">
-					<Icon name="logos:github-copilot" size="48px"/>
-					GitHub Copilot
+				<div class="title--slide-from-bottom" :ref="onElementVisible">
+					<span class="technology__item" style="transition-delay: 2.5s">
+						<Icon name="logos:github-copilot" size="48px"/>
+						GitHub Copilot
+					</span>
 				</div>
 			</div>
 		</div>
